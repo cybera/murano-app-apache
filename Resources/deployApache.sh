bcb19ac4f047fb06e860508fe1b8bfa3222e5265
Cybera Sysadmin <sysadmin@cybera.ca>
2017-01-09 12:54:11 -0700

    centos support added

--- Resources/deployApache.sh
+++ Resources/deployApache.sh
@@ -7,16 +7,26 @@ sudo mkdir /opt/apache_data
 sudo mount $vol /opt/apache_data
 echo "$vol /opt/apache_data ext4 defaults 0 1 " | sudo tee --append  /etc/fstab
 
+if (python -mplatform | grep -qi Ubuntu)
+then #Ubuntu
+  sudo apt-get update
+  sudo apt-get install -y apache2 libapache2-mod-php5
 
-sudo apt-get update
-sudo apt-get install -y apache2 libapache2-mod-php5
+  sudo service apache2 stop
 
-sudo service apache2 stop
+  sudo rsync -av /var/www/html /opt/apache_data
+  sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/apache2/sites-enabled/000-default.conf
+  sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/apache2/sites-available/default-ssl.conf
+  sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/apache2/apache2.conf
 
-sudo rsync -av /var/www/html /opt/apache_data
-sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/apache2/sites-enabled/000-default.conf
-sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/apache2/apache2.conf
+  sudo service apache2 restart
+else #CentOS
+  sudo yum clean all
+  sudo yum -y  update
+  sudo yum -y install httpd mod_ssl
+  sudo rsync -av /var/www/html /opt/apache_data
+  sudo sed -i -e "s|var/www|opt/apache_data|g" /etc/httpd/conf/httpd.conf
+  sudo service httpd restart
 
-sudo service apache2 restart
+fi
 
